57eb981d10bee4ab382767bc76adf21dd2e65b8c
eshux <54838488+eshux@users.noreply.github.com>
2021-01-14 15:51:08 +0200

    deploy

---
+++ precache-manifest.afd3242d0c3ebc01c87cf008c10aa66a.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed5ee505fd9fd5135cb7",
+    "url": "/Rock-Paper-Scissors/css/app.e35a4a14.css"
+  },
+  {
+    "revision": "4069805c1b42ae1e7786",
+    "url": "/Rock-Paper-Scissors/css/chunk-vendors.b0b28440.css"
+  },
+  {
+    "revision": "8cbc67871d9fcce18c6a649c83bd124f",
+    "url": "/Rock-Paper-Scissors/img/bg.8cbc6787.jpg"
+  },
+  {
+    "revision": "46e00e52e657b06e13e7cc94cf060501",
+    "url": "/Rock-Paper-Scissors/img/lizard.46e00e52.svg"
+  },
+  {
+    "revision": "c720f716a88247d7ab0bf8fde811c834",
+    "url": "/Rock-Paper-Scissors/img/lizard.c720f716.png"
+  },
+  {
+    "revision": "bdd1a02545165f6ab1da34f35a7e67d2",
+    "url": "/Rock-Paper-Scissors/img/lizard2.bdd1a025.svg"
+  },
+  {
+    "revision": "038f5b7e71557f8b7240c39cbcae06e5",
+    "url": "/Rock-Paper-Scissors/img/paper.038f5b7e.svg"
+  },
+  {
+    "revision": "d203c331cd9e2ef36ad4c92794874366",
+    "url": "/Rock-Paper-Scissors/img/paper.d203c331.png"
+  },
+  {
+    "revision": "29a1af1637581371832b87a72684dc6f",
+    "url": "/Rock-Paper-Scissors/img/paper2.29a1af16.svg"
+  },
+  {
+    "revision": "bc14c1c12db5864203e6e54a520db9a9",
+    "url": "/Rock-Paper-Scissors/img/qq.bc14c1c1.png"
+  },
+  {
+    "revision": "02de79074d2fc63879e8032f638b7665",
+    "url": "/Rock-Paper-Scissors/img/rock.02de7907.png"
+  },
+  {
+    "revision": "31a18ea89e32bd5de165d1b8d193255d",
+    "url": "/Rock-Paper-Scissors/img/rock.31a18ea8.svg"
+  },
+  {
+    "revision": "8300332b280a63aeb6925c653171b3ff",
+    "url": "/Rock-Paper-Scissors/img/rock2.8300332b.svg"
+  },
+  {
+    "revision": "a43836dec91996c5e7fe273cf3ff74c4",
+    "url": "/Rock-Paper-Scissors/img/rules2.a43836de.svg"
+  },
+  {
+    "revision": "650283c4a7b7a295a61b64df0d27e27c",
+    "url": "/Rock-Paper-Scissors/img/scissors.650283c4.svg"
+  },
+  {
+    "revision": "e4eec66b09eefb0886b38d47a6dd7d0d",
+    "url": "/Rock-Paper-Scissors/img/scissors.e4eec66b.png"
+  },
+  {
+    "revision": "8fa3f50fd7e806637731feef9042e139",
+    "url": "/Rock-Paper-Scissors/img/scissors2.8fa3f50f.svg"
+  },
+  {
+    "revision": "5012f81c8016c60b8c051e1add8d29c8",
+    "url": "/Rock-Paper-Scissors/img/spock.5012f81c.png"
+  },
+  {
+    "revision": "967f2220c4656f6ac52b133172baa6cc",
+    "url": "/Rock-Paper-Scissors/img/spock.967f2220.svg"
+  },
+  {
+    "revision": "0d402a0207c898b5dcab3afa5c6974ad",
+    "url": "/Rock-Paper-Scissors/img/spock2.0d402a02.svg"
+  },
+  {
+    "revision": "1478737ec6b2dbc279d412ab5cc71d59",
+    "url": "/Rock-Paper-Scissors/index.html"
+  },
+  {
+    "revision": "ed5ee505fd9fd5135cb7",
+    "url": "/Rock-Paper-Scissors/js/app.1f4d82f4.js"
+  },
+  {
+    "revision": "4069805c1b42ae1e7786",
+    "url": "/Rock-Paper-Scissors/js/chunk-vendors.bf01d89f.js"
+  },
+  {
+    "revision": "3365dcf4b3ea30707a34b21a5ca48fdb",
+    "url": "/Rock-Paper-Scissors/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/Rock-Paper-Scissors/robots.txt"
+  }
+]);
